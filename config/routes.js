d5f31032af96a8f7b341742ba65a892a8468351d
Salem Hilal <salemhilal@gmail.com>
2013-12-24 01:42:32 -0500

    Added an index page and a test.

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,14 @@
 module.exports = function (app) {
-	// Hello, world!
+	// Ping / pong, for healthchecking
 	app.get('/ping', function (req, res) {
     res.setHeader('Content-Type', 'text/plain');
     res.setHeader('Cache-Control', 'no-cache, no-store, max-age=0');
     res.send('pong');
   });
+
+  // Serve up the index.
+  app.get('/', function (req, res) {
+    res.render('index', {data: "world"})
+  })
 };
 
